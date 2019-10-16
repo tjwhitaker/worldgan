150f018b9c00af47340c55f0ba9327104b1f1fcf
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-16 13:55:11 -0600

    Add puppeteer

---
+++ utilities/map/main.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8000/#8/0/0');
+
+  await browser.close();
+})();
