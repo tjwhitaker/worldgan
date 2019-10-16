a2f72ebf901e0bb7ef93cd4bae90b84b99751a76
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-16 13:58:39 -0600

    Set fort collins as default location in map util

--- utilities/map/main.js
+++ utilities/map/main.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://localhost:8000/#8/0/0');
+  await page.goto('http://localhost:8000/#8/40.5853/-105.0844');
 
   await browser.close();
 })();
