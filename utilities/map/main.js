ed0b9f1e61e0c5b1a75cd373de89c06cb6b5609c
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-27 10:43:14 -0600

    Install puppeteer and test usage

--- utilities/map/main.js
+++ utilities/map/main.js
@@ -5,5 +5,16 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('http://localhost:8000/#8/40.5853/-105.0844');
 
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+
   await browser.close();
 })();
