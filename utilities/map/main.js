27f6f58e408e6c86c4b7c7a246bdecaa67f13b3f
Tim Whitaker <tmwhtkr@gmail.com>
2019-11-02 14:18:50 -0600

    Load random coordinate with puppeteer

--- utilities/map/main.js
+++ utilities/map/main.js
@@ -1,24 +1,36 @@
 const puppeteer = require("puppeteer")
 
-;(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+// Loop some number of times
+for (i = 0; i < 1; i++) {
+  (async () => {
+    // Pick random location coordinates
+    const latitude = Math.random() * 180 - 90
+    const longitude = Math.random() * 360 - 180
 
-  await page.goto("http://localhost:8000/#8/40.5853/-105.0844")
-  await page.waitFor(3000)
-  await page.keyboard.press("h")
-  await page.waitFor(3000)
-  await page.screenshot({ path: "example.png" })
+    // Launch puppeteer
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    }
-  })
-
-  console.log("Dimensions:", dimensions)
-
-  await browser.close()
-})()
+    // Resize and grab screenshot
+    await page.setViewport({ width: 512, height: 512 })
+    await page.goto("http://localhost:8000/#8/" + latitude + "/" + longitude)
+    await page.waitFor(3000)
+    await page.keyboard.press("h")
+    await page.waitFor(3000)
+    await page.screenshot({ path: "example.png" })
+  
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
+      }
+    })
+  
+    console.log("Latitude: ", latitude)
+    console.log("Longitude: ", longitude)
+    console.log("Dimensions:", dimensions)
+  
+    await browser.close()
+  })()
+}
